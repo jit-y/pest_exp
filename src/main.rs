22466164f24c870873caa721b97c3591793ce48f
YUJI takahashi <jit.y.31@gmail.com>
2019-12-29 17:04:03 +0900

    json parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+use pest_exp::parser::*;
+
 fn main() {
-    println!("Hello, world!");
+    let unparsed_file = fs::read_to_string("data.json").expect("cannot read file");
+    let json: JSONValue = parse_json_file(&unparsed_file).expect("unsuccessful parse");
+
+    println!("{}", serialize_jsonvalue(&json));
 }
